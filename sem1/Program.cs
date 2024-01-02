bd7b45564e1e6224b31be8bfec4fdc8b55cf2ae0
Elen S <alexia.18@mail.ru>
2024-01-02 16:41:03 +0300

    Решили две задачи в C#

--- sem1/Program.cs
+++ sem1/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// using System.Math;
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// int a = 5;
+// int b = 40;
+
+// int c = b * b;
+// // int c = Math.Pow(b, 2);
+
+// if (a == c)
+// {
+//     Console.WriteLine("Да, является");
+// }
+// else
+// {
+//     // Если a == c - не верно
+//     Console.WriteLine("Не является");
+// }
+
+int N = 10;
+int i = -N;
+
+while (i <= N)
+{
+    Console.Write(i);
+    Console.Write(' ');
+    i++;
+}
