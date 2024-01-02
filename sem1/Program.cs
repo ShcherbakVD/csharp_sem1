d0676b6865141374eb0f8ad7b5e2318a73a624c7
Elen S <alexia.18@mail.ru>
2024-01-02 17:11:36 +0300

    Решила первый тест из четырёх в ДЗ

--- sem1/Program.cs
+++ sem1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// using System.Math;
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
 
 // int a = 5;
 // int b = 40;
@@ -17,6 +17,7 @@ Console.WriteLine("Hello, World!");
 //     // Если a == c - не верно
 //     Console.WriteLine("Не является");
 // }
+
 
 int N = 10;
 int i = -N;
---
+++ sem1/TestHomework/Program.cs
@@ -0,0 +1,17 @@
+﻿// Тест 1:
+
+int firstNumber = 15;
+int secondNumber = 6;
+
+if (firstNumber == secondNumber)
+{
+    Console.WriteLine("числа равны");
+}
+if (firstNumber < secondNumber)
+{
+    Console.WriteLine("второе число больше");
+}
+if (firstNumber > secondNumber)
+{
+    Console.WriteLine("первое число больше");
+}
